7fbb3458da0ab3c16e05c906ff0a770320c01454
Jordi Nebot <llengot@gmail.com>
2013-07-12 11:19:33 +0200

    Fixed bug

--- web.js
+++ web.js
@@ -5,13 +5,11 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var index = new Buffer(fs.readFileSync('index.html'));
   fs.readFile('index.html', function (err, data) {
     if (err) throw err;
     var index = new Buffer(data);
     response.send(index.toString());
-  });
-  
+  }); 
 });
 
 var port = process.env.PORT || 5000;
