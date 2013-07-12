d58d91e4e23a235f434cbb3c243d2459d61716fd
Jordi Nebot <llengot@gmail.com>
2013-07-12 10:31:18 +0200

    Added buffer

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var index = fs.readFileSync('index.html');
+  var index = new Buffer(fs.readFileSync('index.html'));
   if (index.isBuffer()) {
     response.send(index);
   }
